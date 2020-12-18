f80fd1099563e5e8b5d0d560161d31df079e0e11
Kpure1000 <ktsq2000@126.com>
2020-12-19 01:46:11 +0800

    init: commit project

---
+++ app/src/main/java/com/example/kpureposition/ui/notifications/NotificationsViewModel.java
@@ -0,0 +1,23 @@
+package com.example.kpureposition.ui.notifications;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class NotificationsViewModel extends ViewModel {
+
+    public NotificationsViewModel(){
+
+    }
+
+//    private MutableLiveData<String> mText;
+//
+//    public NotificationsViewModel() {
+//        mText = new MutableLiveData<>();
+//        mText.setValue("This is notifications fragment");
+//    }
+//
+//    public LiveData<String> getText() {
+//        return mText;
+//    }
+}
